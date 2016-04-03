e61fc0befe4449a754cf0be9413233ed0310b2c5
sriravic <srinath.pop@gmail.com>
2016-04-02 21:53:36 -0400

    added point light class

---
+++ include/nori/pointlight.h
@@ -0,0 +1,35 @@
+#ifndef __POINT_LIGHT_H__
+#define __POINT_LIGHT_H__
+
+#pragma once
+#include <nori/emitter.h>
+
+NORI_NAMESPACE_BEGIN
+
+/**
+	This class defines an isotropic point light source that is defined by its position and power.
+*/
+
+class PointLight : public Emitter
+{
+public:
+
+	PointLight(const PropertyList& prop);
+
+	// Methods that have to be implemented for all types of emitters.
+	Color3f sample(EmitterQueryRecord &lRec, const Point2f &sample) const;
+
+	float pdf(const EmitterQueryRecord &lRec) const;
+
+	Color3f eval(const EmitterQueryRecord &lRec) const;
+
+	std::string toString() const;
+
+private:
+	Color3f		m_power;
+	Vector3f	m_position;
+};
+
+NORI_NAMESPACE_END
+
+#endif
---
+++ src/pointlight.cpp
@@ -0,0 +1,38 @@
+#include <nori/pointlight.h>
+
+NORI_NAMESPACE_BEGIN
+
+PointLight::PointLight(const PropertyList& prop)
+{
+	m_power = prop.getColor("power");
+	m_position = prop.getPoint3("position");
+}
+
+Color3f PointLight::sample(EmitterQueryRecord &lRec, const Point2f &sample) const
+{
+	// Explicit sampling of the point light.
+	lRec.p = m_position;
+	lRec.pdf = 1.0f;
+	lRec.wi = (m_position - lRec.ref).normalized();
+	lRec.dist = (m_position - lRec.ref).norm();
+	return m_power * INV_FOURPI / (lRec.dist * lRec.dist);
+}
+
+float PointLight::pdf(const EmitterQueryRecord &lRec) const
+{
+	// We can never sample the pointlight through a random process without explicit connection.
+	return 0.0f;
+}
+
+Color3f PointLight::eval(const EmitterQueryRecord &lRec) const
+{
+	return Color3f(0.0f);
+}
+
+std::string PointLight::toString() const
+{
+	return tfm::format("PointLight[Position={%f,%f,%f}; Power={%f,%f,%f}]", m_position.x(), m_position.y(), m_position.z(), m_power.x(), m_power.y(), m_power.z());
+}
+
+NORI_REGISTER_CLASS(PointLight, "pointlight");
+NORI_NAMESPACE_END

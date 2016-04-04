397630d5ef67cd1c057439aabb2e34206413a30a
sriravic <srinath.pop@gmail.com>
2016-04-04 11:47:27 -0400

    Adding last stage components for assignment1

---
+++ src/directlightingintegrator.cpp
@@ -0,0 +1,56 @@
+#include <nori/bsdf.h>
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/sampler.h>
+
+NORI_NAMESPACE_BEGIN
+
+class DirectLightingIntegrator : public Integrator
+{
+public:
+	DirectLightingIntegrator(const PropertyList& prop) { }
+
+	~DirectLightingIntegrator() { }
+
+	Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+		/* Find the surface that is visible in the requested direction */
+		Intersection its;
+		if (!scene->rayIntersect(ray, its))
+			return Color3f(0.0f);
+
+		/* Intersection found */
+		Color3f Ld(0.0f);
+		const BSDF* bsdf = its.mesh->getBSDF();
+
+		for (auto e : scene->getLights())
+		{
+			// Construct an Emitter query record
+			EmitterQueryRecord eRec;
+			eRec.ref = its.p;
+			eRec.n = its.geoFrame.n;
+			
+			// Get the incoming radiance and create shadow ray.
+			Color3f Li = e->sample(eRec, sampler->next2D());
+			const Ray3f shadow_ray(its.p, eRec.wi, Epsilon, (1.0f - Epsilon) * eRec.dist);
+			Intersection s_isect;
+			if (!scene->rayIntersect(shadow_ray, s_isect))
+			{
+				// If unoccluded to the light source, compute the lighting term and add contributions.
+				BSDFQueryRecord bRec(its.toLocal(-ray.d), its.toLocal(eRec.wi), ESolidAngle);
+				Ld += bsdf->eval(bRec) * Li * std::max(its.geoFrame.n.dot(eRec.wi), 0.0f);
+			}
+		}
+
+		return Ld;
+	}
+
+	std::string toString() const {
+		return "DirectLightingIntegrator[]";
+	}
+
+private:
+
+};
+
+NORI_REGISTER_CLASS(DirectLightingIntegrator, "direct");
+NORI_NAMESPACE_END
--- src/pointlight.cpp
+++ src/pointlight.cpp
@@ -15,6 +15,7 @@ Color3f PointLight::sample(EmitterQueryRecord &lRec, const Point2f &sample) cons
 	lRec.pdf = 1.0f;
 	lRec.wi = (m_position - lRec.ref).normalized();
 	lRec.dist = (m_position - lRec.ref).norm();
+	lRec.emitter = this;
 	return m_power * INV_FOURPI / (lRec.dist * lRec.dist);
 }
 
@@ -34,5 +35,5 @@ std::string PointLight::toString() const
 	return tfm::format("PointLight[Position={%f,%f,%f}; Power={%f,%f,%f}]", m_position.x(), m_position.y(), m_position.z(), m_power.x(), m_power.y(), m_power.z());
 }
 
-NORI_REGISTER_CLASS(PointLight, "pointlight");
+NORI_REGISTER_CLASS(PointLight, "point");
 NORI_NAMESPACE_END

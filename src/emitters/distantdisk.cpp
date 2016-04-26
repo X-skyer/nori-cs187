7fffc2c30a73e9db4dc56f8219b0c8b0b67d6970
sriravic <srinath.pop@gmail.com>
2016-04-26 05:13:52 -0400

    small fix to eval method

--- src/emitters/distantdisk.cpp
+++ src/emitters/distantdisk.cpp
@@ -51,8 +51,8 @@ Color3f DistantDisk::eval(const EmitterQueryRecord & lRec) const
 	// Return radiance only if it's within the accepted limits.
 	Vector3f world_dir = -lRec.wi;
 	Vector3f local_dir = m_worldToLocal * world_dir;
-	float pdf = Warp::squareToUniformSphereCapPdf(local_dir, m_cosThetaMax);
-	if (pdf > 0.0f) return m_radiance;
+	float cos_theta = Frame::cosTheta(local_dir);
+	if (cos_theta >= m_cosThetaMax) return m_radiance;
 	else return 0.0f;
 }
 

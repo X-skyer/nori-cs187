5a2e9beabc02c4dadcc7849dabf21754e168ed17
Srinath Ravichandran <srinath.pop@gmail.com>
2016-04-28 19:56:29 -0400

    fixed untracked files

--- src/integrators/direct_ems.cpp
+++ src/integrators/direct_ems.cpp
@@ -24,17 +24,8 @@ public:
 		Intersection its;
 		if (!scene->rayIntersect(ray, its))
 		{
-			for (auto e : scene->getLights())
-			{
-				if (e->getEmitterType() == EmitterType::EMITTER_DISTANT_DISK)
-				{
-					// get the distant light as of now and return the radiance for the direction
-					EmitterQueryRecord eRec;
-					eRec.wi = ray.d;
-					return e->eval(eRec);
-				}
-			}
-			return Color3f(0.0f);
+			float pdf;
+			return scene->getBackground(ray, sampler->next2D());
 		}
 
 		/* Intersection found */
@@ -62,18 +53,19 @@ public:
 			// Get the incoming radiance and create shadow ray.
 			// Assume Li has the pdf included in it.
 			Color3f Li = e->sample(eRec, sampler->next2D(), sampler->next1D());
-			const Ray3f shadow_ray(its.p, eRec.wi, Epsilon, (1.0f - Epsilon) * eRec.dist);
-			Intersection s_isect;
-			if (!scene->rayIntersect(shadow_ray, s_isect))
+			
+			// Compute BSDF contribution for chosen direction.
+			BSDFQueryRecord bRec(its.toLocal(eRec.wi), its.toLocal(-ray.d), ESolidAngle);
+			Color3f evalTerm = bsdf->eval(bRec) * Li * fmaxf(its.shFrame.n.dot(eRec.wi), 0.0f);
+			
+			if (!evalTerm.isZero() && evalTerm.isValid())
 			{
-				// If unoccluded to the light source, compute the lighting term and add contributions.
-				BSDFQueryRecord bRec(its.toLocal(eRec.wi), its.toLocal(-ray.d), ESolidAngle);
-				Color3f evalTerm = bsdf->eval(bRec) * Li * fmaxf(its.shFrame.n.dot(eRec.wi), 0.0f);
-				if (!evalTerm.isValid())
+				const Ray3f shadow_ray(its.p, eRec.wi, Epsilon, (1.0f - Epsilon) * eRec.dist);
+				Intersection s_isect;
+				if (!scene->rayIntersect(shadow_ray, s_isect))
 				{
-					std::cout << "Invalid term" << std::endl;
+					Ld += evalTerm;
 				}
-				Ld += evalTerm;
 			}
 		}
 

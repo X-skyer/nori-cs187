d6f26049b3a503b294be7b2b510d9c5fd880c340
Srinath Ravichandran <srinath.pop@gmail.com>
2016-05-09 13:37:51 -0400

    added code to silently render without creating a gui

--- src/main.cpp
+++ src/main.cpp
@@ -18,42 +18,105 @@
 
 #include <nori/block.h>
 #include <nori/gui.h>
+#include <nori/optionsparser.h>
+#include <nori/render.h>
 #include <filesystem/path.h>
-
-int main(int argc, char **argv) {
 using namespace nori;
 
-    try {
-    nanogui::init();
+// Launch the gui and render the scene
+int gui_render(int argc, char** argv)
+{
+	try {
+		nanogui::init();
 
-        // Open the UI with a dummy image
-        ImageBlock block(Vector2i(720, 720), nullptr);
-        NoriScreen *screen = new NoriScreen(block);
+		// Open the UI with a dummy image
+		ImageBlock block(Vector2i(720, 720), nullptr);
+		NoriScreen *screen = new NoriScreen(block);
 
-        // if file is passed as argument, handle it
-        if (argc == 2) {
-            std::string filename = argv[1];
-            filesystem::path path(filename);
+		// if file is passed as argument, handle it
+		if (argc == 2) {
+			std::string filename = argv[1];
+			filesystem::path path(filename);
 
-        if (path.extension() == "xml") {
-                /* Render the XML scene file */
-                screen->openXML(filename);
-        } else if (path.extension() == "exr") {
-            /* Alternatively, provide a basic OpenEXR image viewer */
-                screen->openEXR(filename);
-        } else {
-                cerr << "Error: unknown file \"" << filename
-                 << "\", expected an extension of type .xml or .exr" << endl;
-        }
-        }
+			if (path.extension() == "xml") {
+				/* Render the XML scene file */
+				screen->openXML(filename);
+			}
+			else if (path.extension() == "exr") {
+				/* Alternatively, provide a basic OpenEXR image viewer */
+				screen->openEXR(filename);
+			}
+			else {
+				cerr << "Error: unknown file \"" << filename
+					<< "\", expected an extension of type .xml or .exr" << endl;
+			}
+		}
+		nanogui::mainloop();
+		delete screen;
+		nanogui::shutdown();
 
-        nanogui::mainloop();
-        delete screen;
-        nanogui::shutdown();
+	}
+	catch (const std::exception &e) {
+		cerr << "Fatal error: " << e.what() << endl;
+		return -1;
+	}
+	return 0;
+}
 
-    } catch (const std::exception &e) {
-        cerr << "Fatal error: " << e.what() << endl;
-        return -1;
-    }
-    return 0;
+// Don't create a gui
+// Just render it silently
+int silent_render(std::string& filename)
+{
+	try
+	{
+		ImageBlock block(Vector2i(720, 720), nullptr);
+
+		// Just ask render thread to render it
+		RenderThread m_thread(block);
+		m_thread.renderScene(filename);
+		while (m_thread.isBusy())
+		{
+			std::cout << "\rProgress : " << m_thread.getProgress();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		cerr << "Fatal Error : " << e.what() << endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+
+int main(int argc, char **argv) {
+
+	// for now, check if a -s flag is present
+	// if so, call silent code;
+	// NOTE: CRAPPY.!!! TOO MANY BUGS POSSIBLE.!!
+	bool silent = false;
+	std::string filename;
+	for (int i = 0; i < argc; i++)
+	{
+		if (std::string(argv[i]) == "-s")
+		{
+			silent = true;
+			break;
+		}
+		if (std::string(argv[i]) == "-f")
+		{
+			filename = std::string(argv[i + 1]);
+		}
+	}
+
+    // call appropriate function
+	// and return the correct code
+	if (silent)
+	{
+		silent_render(filename);
+	}
+	else
+	{
+		gui_render(argc, argv);
+	}
 }
